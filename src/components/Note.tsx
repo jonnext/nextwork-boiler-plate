269c7987e876f2b8271866012c86ff30b9d96225
jonnext <jon@nextwork.org>
2025-02-04 20:35:33 +1300

    TEST push

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -10,11 +10,7 @@ interface NoteProps {
   maxHeight?: number;
 }
 
-const defaultContent = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-
-Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.
-
-At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.`;
+const defaultContent = `This is a note to test the note component. Wowser!`;
 
 const Note: React.FC<NoteProps> = ({
   text = defaultContent,
